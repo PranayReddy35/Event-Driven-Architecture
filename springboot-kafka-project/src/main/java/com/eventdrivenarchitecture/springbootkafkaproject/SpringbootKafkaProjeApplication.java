edd8e4199f46d2759d6d17a8808c8067d0012816
Pranay Reddy <94630894+PranayReddy35@users.noreply.github.com>
2023-06-22 02:01:19 -0500

    Add files via upload

---
+++ springboot-kafka-project/src/main/java/com/eventdrivenarchitecture/springbootkafkaproject/SpringbootKafkaProjeApplication.java
@@ -0,0 +1,13 @@
+package com.eventdrivenarchitecture.springbootkafkaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaProjeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaProjeApplication.class, args);
+	}
+
+}
---
+++ springboot-kafka-project/src/test/java/com/eventdrivenarchitecture/springbootkafkaproject/SpringbootKafkaProjeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventdrivenarchitecture.springbootkafkaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaProjeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
